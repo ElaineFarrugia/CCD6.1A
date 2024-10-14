9381aed61dc08943dc3cb0022b4ae144723444da
Elaine Farrugia <>
2024-10-14 14:44:35 +0200

    UnityPractice continued...

---
+++ Unity Practice/Assets/KillBall.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) //right mouse click
+        {            
+            Vector3 mousePosition = Input.mousePosition; //get mouse location          
+
+            //convert from screen space to world space
+            Vector3 inWorldSpace = Camera.main.ScreenToWorldPoint(mousePosition);
+            inWorldSpace.z = 0;
+
+            //check if this ball is close enough to the click
+            float distance = Vector3.Distance(inWorldSpace, this.gameObject.transform.position);
+            print(distance);
+
+            if (distance < 0.5)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- Unity Practice/Assets/Spawning.cs
+++ Unity Practice/Assets/Spawning.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spawning : MonoBehaviour
 {
     public GameObject BallPrefab;   //variable declaration
+    int NumOfBalls = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,16 +16,22 @@ public class Spawning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //print(NumOfBalls);
+
         if (Input.GetMouseButtonDown(0)) //0 means left-click
         {
-            Vector3 mousePosition = Input.mousePosition; //get mouse location
+            if (NumOfBalls < 5)
+            {
+                Vector3 mousePosition = Input.mousePosition; //get mouse location
 
-            //convert from screen space to world space
-            Vector3 inWorldSpace = Camera.main.ScreenToWorldPoint(mousePosition);
-            inWorldSpace.z = 0; //make ball between background and camera
+                //convert from screen space to world space
+                Vector3 inWorldSpace = Camera.main.ScreenToWorldPoint(mousePosition);
+                inWorldSpace.z = 0; //make ball between background and camera
 
-            //spawn a ball using the prefab
-            Instantiate(BallPrefab, inWorldSpace, Quaternion.identity);
+                //spawn a ball using the prefab
+                Instantiate(BallPrefab, inWorldSpace, Quaternion.identity);
+                NumOfBalls++;              
+            }
         }
     }
 }
