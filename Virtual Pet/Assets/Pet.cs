5691a8321df5abccfd1361c7a2050b09e200b57d
Elaine Farrugia <>
2024-10-22 10:16:31 +0200

    started Virtual Pet example

---
+++ Virtual Pet/Assets/Pet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet
+{
+    string name;
+    int age;
+    int happinessLevel;
+    int energyLevel;
+
+    public void feed()
+    {
+        energyLevel += 1;
+        happinessLevel += 1;
+    }
+
+    public void play()
+    {
+        energyLevel -= 1;
+        happinessLevel += 1;
+    }
+
+    public void sleep()
+    {
+        energyLevel += 1;       
+    }
+}
